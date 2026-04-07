c2442683ce5bc5234e6aa3f555908491b015eefb
finn-sharp <penguin-klg@naver.com>
2026-04-07 14:16:06 +0900

    ch3 - prompt engineering prompt 내 config를 활용한 엔지니어링 적용, properties로 key, data type정의, required로 필수 key값 정의

--- Github/review-lecture-solution/app/gemini_client.py
+++ Github/review-lecture-solution/app/gemini_client.py
@@ -1,4 +1,5 @@
 from google import genai
+from google.genai import types
 
 import os
 from dotenv import load_dotenv
@@ -6,8 +7,36 @@ load_dotenv()
 
 GEMINI_API = os.getenv("GEMINI_API")
 client = genai.Client(api_key=GEMINI_API)
+
+prompt = """주어진 리뷰 텍스트를 분석해주세요.
+
+리뷰 : 상품 색상이 사진과 너무 달라요.
+
+다음 기준으로 분석하세요 :
+- sentiment : '긍정', '부정', '중립' 중 하나
+- category : '배송', '품질', '가격', '고객서비스', '기타' 중 하나
+- summary : 리뷰 핵심을 1~2문장으로 요약
+- confidence : 0.0 ~ 1.0 사이의 신뢰도"""
+
 response = client.models.generate_content(
     model="gemini-2.5-flash-lite",
-    contents="내용을 입력하시오"
+    contents=prompt,
+
+    # Prompt Engineering의 영역
+    config=types.GenerateContentConfig(
+        # json으로 응답하도록 설정**
+        response_mime_type="application/json",
+        response_schema={
+            "type" : "object",
+            "properties" : {
+                "sentiment" : {"type" : "string"},
+                "category" : {"type" : "string"},
+                "summary" : {"type" : "string"},
+                "confidence" : {"type" : "number"}
+            },
+            "required" : ["sentiment", "category", "summary", "confidence"]
+        }
+    )
+    
 )
-print(response)
+print(response.text)
